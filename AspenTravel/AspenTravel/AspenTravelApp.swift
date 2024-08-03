749bab9481e6f01c238b7b474939ea57c012c77b
Nadiia Vozna <charivnist@icloud.com>
2024-08-04 01:20:47 +0300

    Was created project and added first image from design

---
+++ AspenTravel/AspenTravel/AspenTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AspenTravelApp.swift
+//  AspenTravel
+//
+//  Created by Kluf on 04.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AspenTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
